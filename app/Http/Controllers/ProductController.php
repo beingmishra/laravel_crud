5938dc3586f646f40a0a14282e5ac1746fd7a152
Rahul <Rahul731999@gmail.com>
2024-09-30 18:39:44 +0530

    Add delete operation

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -66,4 +66,11 @@ class ProductController extends Controller
         return back()->withSuccess("Product updated successfully!");
     }
 
+
+    public function delete($id) {
+        $product = Product::where('id', $id)->first();
+        $product->delete();
+        return back()->withSuccess("Product deleted successfully!");
+    }
+
 }
